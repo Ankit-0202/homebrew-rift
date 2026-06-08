596c01f0e436b02237bbda8fc1405010c932bcee
Ankit Kapoor <ankitkap2002@gmail.com>
2026-06-09 00:53:50 +1000

    Refresh the 0.1.0 binary checksums

--- Formula/rift.rb
+++ Formula/rift.rb
@@ -7,14 +7,14 @@ class Rift < Formula
   on_macos do
     on_arm do
       url "https://github.com/Ankit-0202/rift/releases/download/v0.1.0/rift-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "55260c745ad2255a86c44f3d80e37140171d299f823715bdfcf4cf62b6dc741a"
+      sha256 "921922fed1d17009d47e5a9d491220bddac524e7ab48399fe22a070611ccf007"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/Ankit-0202/rift/releases/download/v0.1.0/rift-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "92833c95bdaf1ffc08ff1e54646b7c3a4bfe1acfdc66714767cfd6c584cefe1b"
+      sha256 "44cbbcf8be5b5f653288423e4086474c49a89e92134e40b1f603d5dff9862228"
     end
   end
 
